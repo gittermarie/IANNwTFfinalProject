d07f4ddd7e04f793e6d611c0e77db6b5307aec4d
Marta Sokol <marta.katarzyna.sokol@gmail.com>
2023-03-22 10:43:12 +0100

    Working model

--- loss.py
+++ loss.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from keras.backend import epsilon
+import tensorflow.python.keras.backend as K
+
 
 class WeightedCrossEntropyBinaryLoss:
   """
@@ -7,14 +10,21 @@ class WeightedCrossEntropyBinaryLoss:
   def __init__(self, zero_weight, one_weight):
     self.zero_weight = zero_weight
     self.one_weight = one_weight
-  
-  def weighted_binary_crossentropy(self, y, yhat):
 
-    # Compute the weighted crossentropy binary loss
-    fuzzy_factor = tf.convert_to_tensor(tf.keras.backend.epsilon(), dtype=yhat.dtype.base_dtype) # fuzzy factor to make calculations more accurate
-    yhat = tf.clip_by_value(yhat, fuzzy_factor, 1 - fuzzy_factor)
-    weighted_bce = -self.one_weight * y * tf.math.log(yhat) \
-        -self.zero_weight * (1 - y) * tf.math.log(1 - yhat)
+  def weighted_binary_crossentropy(y_true, y_pred):
+    y_true = K.cast(y_true, dtype=tf.float32)
+
+    epsilon = tf.keras.backend.epsilon()
+    y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
+
+    # Compute cross entropy from probabilities.
+    bce = y_true * tf.math.log(y_pred + epsilon)
+    bce += (1 - y_true) * tf.math.log(1 - y_pred + epsilon)
+    bce = -bce
+
+    # Apply the weights to each class individually
+    weight_vector = y_true * self.one_weight + (1. - y_true) * self.zero_weight
+    weighted_bce = weight_vector * bce
 
     # Return the mean error
     return tf.reduce_mean(weighted_bce)
--- prepare_data.py
+++ prepare_data.py
@@ -1,30 +1,18 @@
 ### The dataset can be downloaded from here:
 ### https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia
 
-# General libraries
+
 import os
 import numpy as np
-import pandas as pd 
-import random
 import cv2
 import matplotlib.pyplot as plt
-#%matplotlib inline
-
-# Deep learning libraries
-import keras.backend as K
-from keras.models import Model, Sequential
-from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
-from keras.layers import Conv2D, SeparableConv2D, MaxPool2D, LeakyReLU, Activation
-from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 import tensorflow as tf
 
-# Setting seeds for reproducibility
+# Setting seeds for the generator
 seed = 232
 np.random.seed(seed)
 tf.random.set_seed(seed)
-
 
 input_path = 'C:/Users/marta/OneDrive/Desktop/Osnabruck/ImplementingANNswithTensorFlow/FinalProject/chest_xray/'
 
